fd2bbd21506596893e02c30fa8ba019383db8d20
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-11-19 16:46:22 +0530

    More Pages Added

--- components/DataBox.js
+++ components/DataBox.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Twitter from "./twitter.png";
 import Discord from './discord.png';
-const DataBox = ({platform,pic}) => {
+import Link from 'next/link';
+const DataBox = ({platform,funcType,desc,isgiveaway,}) => {
     const login =()=> {
         if(platform=='Discord'){
             alert('We will be Soon on Discord')
         }
-        console.log('signin');
+        console.log();
     }
   return (
     <div className="w-[260px] h-[300px] bg-[#1F2937] text-white rounded-lg text-center flex flex-col justify-center cursor-pointer">
 <h1 className="text-xl text-center">{platform}</h1>
+<h1 className="text-xl text-center">{funcType}</h1>
+
 {/* <img src={platform=="Twitter"?Twitter:Discord} alt="" width="30px" height="30px" className=""/> */}
-<div className="bg-[#D8FF69] w-[100px] h-[30px] p-1 rounded-lg text-black absolute top-[185px] left-[85px]" onClick={login}>Login </div>
+<div className="bg-[#D8FF69] w-[100px] h-[30px] p-1 rounded-lg text-black absolute top-[215px] left-[85px]" >{isgiveaway?"Create" : 'Login'}</div>
+<p className="text-lg text-white">You can create a giveaway for your base and distribute the prize on $SOL</p>
     </div>
   )
 }
---
+++ pages/giveaway.js
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+import Sidenav from "../components/Sidenav";
+export default function Giveaway(){
+return <div>
+    <Navbar/>
+    <Sidenav />
+    
+</div>
+}
